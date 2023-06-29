9781474ffb32e0b679976b9ccc2fca604f6e1763
didierthomasm <didierthomas.m@gmail.com>
2023-06-29 12:04:37 -0600

    added checkboxes in the html, edited the css to center the checkboxes and finished the readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # JS-Awesome-Password-Generator
+
+With the **JS-Awesome-Password-Generator** you won't need to worry anymore for have secured passwords, with a few clicks you can choose between options to fill your needs.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+- [Deployment](#deployment)
+
+## Features
+
+- Generate random passwords with customizable criteria.
+- Select password length between 8 and 128 characters.
+- Include or exclude lowercase, uppercase, numeric, and special characters.
+- Validation and error handling for user inputs.
+
+## Installation
+
+1. Clone the repository from  or download the source code.
+2. Open the `index.html` file in a web browser.
+
+## Usage
+
+1. Open the web app in a web browser.
+2. Select the desired criteria for the password.
+3. Click on the "Generate Password" button.
+4. A awesome random password will be generated and displayed in the textarea.
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements, please feel free to submit a pull request or create an issue in the repository.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contact
+
+If you have any questions or want to connect, feel free to reach out to me:
+
+- Email: didierthomas.m@gmail.com
+- Github: [didierthomasm](https://github.com/didierthomasm/)
+
+
+## Deployment
+<https://didierthomasm.github.io/js-awesome-password-generator>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,76 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to generate a random password
+  function generatePassword() {
+    // Get the password length
+    var passwordLength = parseInt(document.getElementById('password-length').value);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // Validate the password length
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Invalid password length. Please enter a number between 8 and 128.");
+      return;
+    }
 
-  passwordText.value = password;
+    // Check for character types
+    var includeLowercase = document.getElementById("include-lowercase").checked;
+    var includeUppercase = document.getElementById("include-uppercase").checked;
+    var includeNumeric = document.getElementById("include-numeric").checked;
+    var includeSpecial = document.getElementById("include-special").checked;
 
-}
+    // Validate that at least one character type is selected
+    if (
+      !includeLowercase &&
+      !includeUppercase &&
+      !includeNumeric &&
+      !includeSpecial
+    ) {
+      alert("Please select at least one character type.");
+      return;
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // Define character pools based on selected character types
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericChars = "0123456789";
+    var specialChars = "!@#$%^&*()_+~`|}{[]\\:;?><,./-=";
+
+    var characters = "";
+    // Add selected character types to the pool
+    if (includeLowercase) {
+      characters += lowercaseChars;
+    }
+
+    if (includeUppercase) {
+      characters += uppercaseChars;
+    }
+
+    if (includeNumeric) {
+      characters += numericChars;
+    }
+
+    if (includeSpecial) {
+      characters += specialChars;
+    }
+
+    // Generate the password
+    var password = "";
+
+    for (var i = 0; i < passwordLength; i++) {
+
+      var randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+    return password;
+
+    
+
+  }
+  // Function that displays the generated password
+  function displayPassword() {
+    var password = generatePassword();
+    var passwordField = document.getElementById("password");
+    passwordField.textContent = password;
+  }
+
+  // Call the displayPassword function when the button is clicked
+  document.getElementById("generate").addEventListener("click", displayPassword);
+});
